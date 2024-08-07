0c32288a615aa65c5cc64f1d10a6fc8aeedbcc0f
Maxim Gladkov <maxdgladkov@gmail.com>
2024-08-07 15:26:33 +0300

    handler, storage, server start

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,22 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/hddskull/practicus/internal/handler"
+)
+
+func start(mux *http.ServeMux) {
+	err := http.ListenAndServe(":8080", mux)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/update/", handler.UpdateMetrics)
+
+	start(mux)
+}
